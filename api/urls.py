04725b49be09106bf50c470ca1294a938365ff28
Julio Briones <Julio_Briones@epam.com>
2022-08-10 13:14:27 -0500

    Finished implementing RetrieveUpdateDestroy view.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ProductList, ProductCreate, ProductDelete
+from .views import ProductList, ProductCreate, ProductRetrieveUpdateDestroy
 
 app_name = "api"
 
 urlpatterns = [
     path("v1/products/", ProductList.as_view(), name="list-products"),
     path("v1/products/new", ProductCreate.as_view(), name="create-product"),
-    path("v1/products/<int:id>/delete", ProductDelete.as_view(), name="delete-product"),
+    path("v1/products/<int:id>/", ProductRetrieveUpdateDestroy.as_view(), name="update-delete-product"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
+from django.core.cache import cache
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -51,15 +53,26 @@ class ProductCreate(CreateAPIView):
         return super().create(request, *args, **kwargs)
 
 
-class ProductDelete(DestroyAPIView):
+class ProductRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     lookup_field = "id"
+    serializer_class = ProductSerializer
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            product = response.data
+            cache.set(f"product_data_{product['id']}", {
+                "name": product.get("name"),
+                "description": product.get("description"),
+                "price": product.get("price"),
+            })
+        return response
 
     def delete(self, request, *args, **kwargs):
         product_id = request.data.get("id")
         response = super().delete(request, *args, **kwargs)
         if response.status_code == 204:
             # Clear product from the cache
-            from django.core.cache import cache
             cache.delete(f"product_data_{product_id}")
         return response
